32a8bc46e448d9e38122587ae88fa65309921b96
3635973600 <3635973600@qq.com>
2026-02-15 23:36:01 +0800

    1

--- source/samlle_dog/samlle_dog/tasks/manager_based/go1/velocity_env_cfg.py
+++ source/samlle_dog/samlle_dog/tasks/manager_based/go1/velocity_env_cfg.py
@@ -327,7 +327,7 @@ class RewardsCfg:
             "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*foot"),
             "command_name": "base_velocity",
             "threshold": 0.3,
-            "max_time": 0.8,
+            "max_time": 0.5,
         },
     )
     
@@ -372,7 +372,7 @@ class RewardsCfg:
         params={
             "command_name": "base_velocity",
             "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*foot"),
-            "max_time": 1.0,
+            "max_time": 0.7,
             "threshold": 1.0,
         },
     )
